358eda61edea97b51e2bb29cef0684e90a8147f8
umedahiroki <69580353+umedahiroki@users.noreply.github.com>
2020-09-22 17:59:08 +0900

    編集画面実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:'books#index'
-  resources :books, only: [:index,:new,:create,:show,:destroy,:edit]
+  resources :books, only: [:index,:new,:create,:show,:destroy,:edit,:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
