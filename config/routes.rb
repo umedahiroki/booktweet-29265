3027db4b6f2ce8a01b01fa562e9582e37a783fd2
umedahiroki <69580353+umedahiroki@users.noreply.github.com>
2020-09-28 19:21:05 +0900

    コメント機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to:'books#index'
   resources :books do
+    resources :comments, only: :create
     collection do
       get 'search'
     end
