6ca85f2ab676aa9212b49a9020b83735e02456dd
umedahiroki <69580353+umedahiroki@users.noreply.github.com>
2020-10-09 19:29:03 +0900

    マイページを最新情報から表示する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
     def show
         user = User.find(params[:id])
-        @books = user.books
+        @books = user.books.order("created_at DESC")
     end
 end
