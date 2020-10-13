55b1b6cadbad2b5653a251255990a87a6acea55d
umedahiroki <69580353+umedahiroki@users.noreply.github.com>
2020-10-13 17:40:57 +0900

    jqueyの導入

---
+++ app/javascript/namecolor.js
@@ -0,0 +1,8 @@
+$('#user-acount').hover(
+    function(){
+        $('#user-acount').find('li').css('color','red');
+    },
+    function(){
+        $('#user-acount').find('li').css('color','red');
+    }
+);
